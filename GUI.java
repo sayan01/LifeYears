efd1357a7e1b23d4e0bfb44a50ac82ad8164a0ec
Sayan Ghosh <sgic2001@gmail.com>
2019-05-05 23:44:57 +0530

    Removed non green buttons; Removed button border; Made last button gradient; Refacotred code.

--- GUI.java
+++ GUI.java
@@ -6,35 +6,33 @@ class GUI extends JFrame{
 	final int frameW = 480;
 	final int frameH = 480;
 	static ImageIcon icon = new ImageIcon("icon.png");
+	static ImageIcon green = new ImageIcon("green.png");
+	static ImageIcon greenT = new ImageIcon("green_trail.png");
 	GUI(){
-		setIconImage(icon.getImage());
-	}
-	GUI(int years){
 		super("LifeYears");
 		setIconImage(icon.getImage());
-		set(years);
-	}
-	void set(int years){
 		setSize(frameW+8,frameH+64);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setLayout(null);
 		setResizable(false);
-		
-		ImageIcon green = new ImageIcon("green.png");
-		JButton[] yearBox = new JButton[100];
+	}
+	GUI(int years){
+		this();
+		set(years);
+	}
+	void set(int years){
+		JButton[] yearBox = new JButton[years];
 		int buttonW = frameW/10;
 		int buttonH = frameH/10;
 		int buttonOffset = 32;
-		for(int i = 0; i < 100;i++){
-			JButton temp = new JButton(green);
+		for(int i = 0; i < years;i++){
+			JButton temp = new JButton((i<years-1)?green:greenT);
 			temp.setSize(buttonW,buttonH);
 			temp.setLocation((i/10)*buttonW,(i%10)*buttonH + buttonOffset);
-			if(i>=years)
-				temp.setEnabled(false);
+			temp.setBorder(null);
 			add(temp);
 			yearBox[i] = temp;
 		}
-
 		JLabel lbl = new JLabel("No. of Years: "+years);
 		lbl.setBounds(0,0,480,32);
 		lbl.setHorizontalAlignment(SwingConstants.CENTER);
